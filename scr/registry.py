4f33044dee85db4f2694e0c96a32103e4ffb274f
CryptoKuper <mainingrafael@gmail.com>
2025-04-26 12:31:24 +0500

    Внесены корректировки

--- scr/registry.py
+++ scr/registry.py
@@ -1,19 +1,19 @@
 class Registry:
     def __init__(self):
-        self._animals = []
+        self.animals = []  # Инициализация списка животных
 
     def add_animal(self, animal):
-        self._animals.append(animal)
+        self.animals.append(animal)
 
     def list_commands(self, name):
-        for animal in self._animals:
+        for animal in self.animals:
             if animal._name == name:
                 return animal.get_commands()
         return "Животное не найдено."
 
     def train_animal(self, name, command):
-        for animal in self._animals:
+        for animal in self.animals:
             if animal._name == name:
                 animal.add_command(command)
-                return f"{name} обучен: {command}"
+                return f"{name} обучен команде: {command}"
         return "Животное не найдено."
